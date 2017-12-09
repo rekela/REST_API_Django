349f771e1e557a318086af5fcdbcb4dd5cda517c
Ela Reksa <ela.kawczak@gmail.com>
2017-12-09 17:46:52 +0100

    views

--- movies/views.py
+++ movies/views.py
@@ -12,18 +12,24 @@ from django.http import Http404
 
 class MoviesView(APIView):
 
-	def get(self, request, format=None): # format nie jest potrzebnym argumentem, możemy go nie wpisywać, chodzi o format danych jakie są przesyłane np. za pomocą API
+	def get(self, request, format=None):
 		movies = Movie.objects.all()
 		serializer = MovieSerializer(movies, many=True)
 		return Response(serializer.data)
 
+	def post(self, request, format=None):
+		serializer = MovieSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class MovieView(APIView):
 
-	def get_object(self, movie_id): # get_object to funkcja pomocnicza stosowana po to, aby pobrać obiekt z bazy
-		try:					# i zwrócić obiekt, jeśli istnieje, a jeśli nie - zwróci 404
-			return 	Movie.objects.get(pk=movie_id) # f.get_object stosujemy po to żeby nie powtarzac poniżej w GET i DELETE tego samego tylko odwoływac się do tej funkcji
+	def get_object(self, movie_id):
+		try:					
+			return 	Movie.objects.get(pk=movie_id)
 		except Movie.DoesNotExist:
 			raise Http404
 
@@ -44,7 +50,4 @@ class MovieView(APIView):
 			serializer.save()
 			return Response(serializer.data)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
 
